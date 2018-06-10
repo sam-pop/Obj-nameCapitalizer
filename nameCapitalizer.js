b727561882fff16de103fe4af161ef3304a38a5c
sam-pop <poplovitch.samuel@gmail.com>
2018-06-10 16:23:59 -0400

    comment added

--- nameCapitalizer.js
+++ nameCapitalizer.js
@@ -4,6 +4,8 @@ Obj-nameCapitalizer:
 This simple tool takes a `.txt` file that contains objects and properly capitalize names in it. 
 Outputs a new converted `.txt` file.
 
+Only supports single-quotes in the input (object containing) `.txt` file.
+
 Built to be used with my randsGenerator tool (but can be used without it with minor code tweaking).
 --------------
 Author: Samuel Poplovitch
@@ -19,33 +21,35 @@ if (fileName) {
 	});
 
 	rl.on('line', function (line) {
-		// uses 'name' as the key to the value to be changed (assuming objects containing lines of this structure: `name: 'john doe',`)
-		if (line.indexOf('name') !== -1) {
-			let vStart = line.indexOf("'");
-			//splits the line to the key and values
-			let value = line.substr(vStart + 1);
-			let key = line.substr(0, line.length - value.length);
-			let vConverted = capitalizeFirstLetter(value);
-			writeOut(key + vConverted);
-		} else writeOut(line);
-	});
-} else console.log("Please enter file name as an arg");
-
-function capitalizeFirstLetter(str) {
-	str = str.toLowerCase();
-	var cArr = [];
-	var splitStr = str.split(" ");
-	for (var i = 0; i < splitStr.length; i++) {
-		cArr.push(splitStr[i][0].toUpperCase() + splitStr[i].slice(1));
+			// uses 'name' as the key to the value to be changed (assuming objects containing lines of this structure: `name: 'john doe',`)
+			if (line.indexOf('name') !== -1) {
+				if (line.indexOf("'" !== -1) let vStart = line.indexOf("'");
+					else let vStart = line.indexOf("\"");
+					//splits the line to the key and values
+					let value = line.substr(vStart + 1);
+					let key = line.substr(0, line.length - value.length);
+					let vConverted = capitalizeFirstLetter(value); writeOut(key + vConverted);
+				}
+				else writeOut(line);
+			});
 	}
-	return cArr.join(" ");
-}
+	else console.log("Please enter file name as an arg");
 
-function writeOut(line) {
-	fs.appendFileSync("./converted_" + fileName, line + '\r\n', (error) => {
-		if (error) throw error;
-	});
-}
+	function capitalizeFirstLetter(str) {
+		str = str.toLowerCase();
+		var cArr = [];
+		var splitStr = str.split(" ");
+		for (var i = 0; i < splitStr.length; i++) {
+			cArr.push(splitStr[i][0].toUpperCase() + splitStr[i].slice(1));
+		}
+		return cArr.join(" ");
+	}
 
-if (fileName)
-	console.log("Converting: " + fileName);
+	function writeOut(line) {
+		fs.appendFileSync("./converted_" + fileName, line + '\r\n', (error) => {
+			if (error) throw error;
+		});
+	}
+
+	if (fileName)
+		console.log("Converting: " + fileName);
